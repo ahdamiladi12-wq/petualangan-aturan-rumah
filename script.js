083512717c704fb0b7ae4d429329330d3cc8f34c
ahdamiladi12-wq <ahdamiladi12@gmail.com>
2026-01-18 13:54:48 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function nextPage(page) {
+  window.location.href = page;
+}
+
+function showInfo() {
+  document.getElementById("info").classList.toggle("hidden");
+}
+
+function showAkibat() {
+  document.getElementById("akibat").classList.remove("hidden");
+}
